91064e7149365a560dd046b838b4078d449ea2e5
rushdina <redredrushrush@gmail.com>
2026-02-03 00:52:23 +0800

    Added fetchStockData shared function in StockContext to pass to child components

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import "./App.css";
 import StockForm from "./components/StockForm.jsx";
 import StockList from "./components/StockList.jsx";
@@ -19,12 +19,38 @@ function App() {
   ]
   */
 
+  // if have current price meaning the symbol is valid
+  const fetchStockData = useCallback((symbol) => {
+    const API_KEY = "42ZEWT4IRU5YGZ8I";
+    return fetch(
+      `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${API_KEY}`,
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (!data["Global Quote"] || !data["Global Quote"]["05. price"]) {
+          return null; // resolves with null if symbol invalid
+        } else {
+          return {
+            symbol,
+            currentPrice: parseFloat(data["Global Quote"]["05. price"]),
+          }; // resolves with valid object
+        }
+      })
+      .catch((error) => {
+        console.error("Invalid symbol:", symbol, error);
+        return null; // resolves with null if fetch fails
+      });
+  }, []);
+
   function addStock(newStock) {
     setStocks((prevStockArray) => [...prevStockArray, newStock]);
   }
 
   return (
-    <StockContext.Provider value={{ stocks, setStocks, addStock }}>
+    <StockContext.Provider
+      value={{ stocks, setStocks, addStock, fetchStockData }}
+    >
       <div className="app">
         <h1 className="page-title heading">Finance Dashboard</h1>
         <StockForm />
