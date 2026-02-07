409d974e731388db2d19844102a3c793d9398ab1
rushdina <redredrushrush@gmail.com>
2026-02-08 00:32:29 +0800

    Added function to validate symbol to get price

--- my-finance-dashboard/src/App.jsx
+++ my-finance-dashboard/src/App.jsx
@@ -30,42 +30,42 @@ function App() {
       const primaryUrl = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${API_KEY}`;
       const demoUrl = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=demo`;
 
+      // Validate symbol to get current price
+      function validateSymbolAndGetPrice(data) {
+        // ?. is optional chaining operator, checks if data exists, then checks data["Global Quote"], then checks data["Global Quote"]["05. price"]
+        // if at any point is null/undefined, returns undefined
+        const priceString = data?.["Global Quote"]?.["05. price"];
+        const price = parseFloat(priceString); // parseFloat(undefined) = NaN
+        return isNaN(price) || price === 0 ? null : price; // return null for invalid symbol
+      }
+
       return fetch(primaryUrl)
         .then((res) => res.json())
         .then((data) => {
           console.log(data); // to check the API rate limit response
 
-          // if primaryURL API fail
+          // if primaryURL API fail (rate limit)
           if (data.Information || data.Note || !data["Global Quote"]) {
             return fetch(demoUrl)
               .then((res) => res.json())
               .then((demoData) => {
-                // Validate symbol for StockForm: a valid symbol has a current price
-                if (
-                  demoData["Global Quote"] &&
-                  demoData["Global Quote"]["05. price"]
-                ) {
-                  console.log(demoData);
-                  return parseFloat(demoData["Global Quote"]["05. price"]); // resolves with price
-                } else {
-                  return null; // resolves with null if symbol is invalid
-                }
+                const price = validateSymbolAndGetPrice(demoData);
+                return price;
               })
               .catch((error) => {
-                console.error(error);
+                console.error("Demo API fetch failed:", error);
                 return null; // resolves with null if fetch fails
               });
           }
 
           // if primaryURL API success
-          else if (data["Global Quote"] && data["Global Quote"]["05. price"]) {
-            return parseFloat(data["Global Quote"]["05. price"]); // valid symbol, return price
-          } else {
-            return null; // invalid symbol, return null
+          else {
+            const price = validateSymbolAndGetPrice(data);
+            return price;
           }
         })
         .catch((error) => {
-          console.error(error);
+          console.error("Primary API fetch failed:", error);
           return null;
         });
     },
